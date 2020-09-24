c07edb5bb2aeac8b9478af12f3a3447ff4037eda
Samuel Gaborieau <sam.gabor@hotmail.com>
2020-09-24 09:53:22 +0200

    complet unit test of handler arg at ./test/args.test.js

--- test/args.test.js
+++ test/args.test.js
@@ -1,29 +1,61 @@
-const { assert, expect } = require('chai');
+const { assert } = require('chai');
 
 const CliArg = require('./../.bin/lib/args');
 
-const [factoryArgvHydrate, indexGenerated] = require('./factory-data/handler-argv/hydrate')({ });
-const [factoryArgvNotHydrate,] = require('./factory-data/handler-argv/hydrate')( { excludeValue: indexGenerated } );
+const generator = require('./factory-data/handler-argv/hydrate');
+
+const [factoryArgvHydrate, indexGenerated] = generator({ });
+const [factoryArgvNotHydrate,] = generator( { excludeValue: indexGenerated } );
+
+const [ factoryOptions, indexOptions ] = generator({
+  withOptions: true
+});
+
+const [factoryOptionsNotHydrate,] = generator({
+  withOptions: true,
+  excludeValue: indexOptions
+});
+
+// save current argv for re affect after test suited
+process.__INIT_ARGV__ = process.argv;
+
+// re affect default argv
+process.argv = process.argv.slice( 0, 2 );
 
 describe('class: "./.bin/lib/args.js"', () => {
 
-  // hydrate argv
+  // re affect init argv
+  after( () => {
 
-  // re affect default argv
-  process.argv = process.argv.slice( 0, 2 );
+    process.argv = process.__INIT_ARGV__;
+    delete process.__INIT_ARGV__;
 
-  // push random argv
-  process.argv = [...process.argv, ...factoryArgvHydrate];
+  } );
 
   describe('is exists argument by name', () => {
+
+    before( () => {
+
+      // push random argv
+      process.__DEFAULT_ARGV__ = process.argv;
+      process.argv = [...process.argv, ...factoryArgvHydrate];
+
+    } );
+
+    after( () => {
+      process.argv = process.__DEFAULT_ARGV__;
+      delete process.__DEFAULT_ARGV__;
+    } );
 
     const handlerArgs = new CliArg({
       preNormalize: argv => argv
     });
 
-    process.argv.forEach( realArg => {
+    factoryArgvHydrate.forEach( realArg => {
 
-      it( 'should exists', () => {
+      const message = `should exists argument: "${realArg}"`;
+
+      it( message, () => {
 
         assert.isTrue( !!handlerArgs.isExistsArg( realArg ) );
 
@@ -53,7 +85,9 @@ describe('class: "./.bin/lib/args.js"', () => {
     // because else should parsed regex builded from string
     process.argv.slice( 2, ).forEach( realArg => {
 
-      it( 'should exists', () => {
+      const message = `should exists argument: "${realArg}"`;
+
+      it( message, () => {
 
         realArg = new RegExp( `^${realArg}$`);
 
@@ -77,4 +111,100 @@ describe('class: "./.bin/lib/args.js"', () => {
 
   } );
 
+  describe('is exists options by name', () => {
+
+    before( () => {
+
+      process.__DEFAULT_ARGV__ = process.argv;
+      process.argv = factoryOptions;
+    } );
+
+    after( () => {
+
+      process.argv = process.__DEFAULT_ARGV__;
+      delete process.__DEFAULT_ARGV__;
+
+    } );
+
+    const handlerArgs = new CliArg({
+      preNormalize: argv => argv
+    });
+
+    factoryOptions.forEach( optionName => {
+
+      const message = `should exists option: "${optionName}"`;
+
+      it( message, () => {
+
+        assert.isTrue(!!handlerArgs.isExistsOption( optionName ));
+
+      } );
+
+    } );
+
+    factoryOptionsNotHydrate.forEach( optionName => {
+
+      const message = `should not exists option: "${optionName}"`;
+
+      it( message, () => {
+
+        assert.isFalse(!!handlerArgs.isExistsOption( optionName ));
+
+      } );
+
+    } );
+
+  } );
+
+  describe('is exists options by pattern', () => {
+
+    before( () => {
+
+      process.__DEFAULT_ARGV__ = process.argv;
+      process.argv = factoryOptions;
+    } );
+
+    after( () => {
+
+      process.argv = process.__DEFAULT_ARGV__;
+      delete process.__DEFAULT_ARGV__;
+
+    } );
+
+    const handlerArgs = new CliArg({
+      preNormalize: argv => argv
+    });
+
+    factoryOptions.forEach( optionName => {
+
+      const message = `should exists option: "${optionName}"`;
+
+      it( message, () => {
+
+        const optionregex = new RegExp( `^${optionName}$`, "i" );
+        assert.isTrue(!!handlerArgs.isExistsOptionByPattern( optionregex ));
+
+      } );
+
+    } );
+
+    factoryOptionsNotHydrate.forEach( optionName => {
+
+      const message = `should not exists option: "${optionName}"`;
+
+      it( message, () => {
+
+        const optionregex = new RegExp( `^${optionName}$`, "i" );
+
+        assert.isFalse(!!handlerArgs.isExistsOptionByPattern( optionregex ));
+
+      } );
+
+    } );
+
+  } );
+
 } );
+
+
+// - **Finish** Unit test for: ./test/args.test.js *(should coverage methods: getOptionsByPattern, isExistsOption )*
